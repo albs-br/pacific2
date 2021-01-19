78178e3fee651b9d2fd063e13215f4283990ebde
Andre Baptista <albs_br@yahoo.com.br>
2021-01-19 17:38:08 -0300

    v.1.32.0

--- gamelogic/enemylogic.s
+++ gamelogic/enemylogic.s
@@ -3,7 +3,7 @@ GameLogicEnemy:
 
 	; ld a, (Enemy_1_Show)			; if enemy 1 is hidden, skip enemy logic/check collision
 	ld 		a, (ix)			; if enemy 1 is hidden, skip enemy logic/check collision
-	cp 		0
+	or		a  ;same as cp 0
 	jp 		z, .skipCheckEnemy_1
 
 	; update enemy position
@@ -15,7 +15,7 @@ GameLogicEnemy:
 	jp 		z, .enemyType_0
 
 	;elseif(enemytype == 1)
-	dec 	a ; c	p 1
+	dec 	a ; cp 1
 	jp 		z, .enemyType_1
 
 	;elseif(enemytype == 2)
@@ -32,64 +32,65 @@ GameLogicEnemy:
 
 .enemyType_0:
 	; ld a, (Enemy_1_Y)				;
-	ld a, (ix + 6)				;
-	inc a
-	cp 192
-	call z, .hideEnemy
+	ld 		a, (ix + Struct_Enemy.Y)				;
+	inc 	a
+	cp 		192
+	call	z, .hideEnemy
 
 	; ld (Enemy_1_Y), a				; save updated value
-	ld (ix + 6), a				; save updated value
+	ld 		(ix + Struct_Enemy.Y), a				; save updated value
 
-	push ix
-    ld ix, EnemyTemp_CollisionBox		; update coord of collision box
-    ld (ix + Struct_CollisionBox.Y), a
-	pop ix
+	push 	ix
+    ld 		ix, EnemyTemp_CollisionBox		; update coord of collision box
+    ld 		(ix + Struct_CollisionBox.Y), a
+	pop 	ix
 
-	jp .contGameLogic
+	jp 		.contGameLogic
 
 .enemyType_1:
 	; ld a, (Enemy_1_X)				;
-	ld a, (ix + 5)				;
-	dec a
-	cp 0
-	call z, .hideEnemy
+	; TODO: optimization here: dec (ix + n)
+	ld 		a, (ix + Struct_Enemy.X)				;
+	dec 	a
+	;cp 0
+	call 	z, .hideEnemy
 
 	; ld (Enemy_1_X), a				; save updated value
-	ld (ix + 5), a				; save updated value
+	ld 		(ix + Struct_Enemy.X), a				; save updated value
 
-	push ix
-    ld ix, EnemyTemp_CollisionBox		; update coord of collision box
-    ld (ix + Struct_CollisionBox.X), a
-	pop ix
+	push 	ix
+    ld 		ix, EnemyTemp_CollisionBox		; update coord of collision box
+    ld 		(ix + Struct_CollisionBox.X), a
+	pop 	ix
 
-	jp .contGameLogic
+	jp 		.contGameLogic
 
 .enemyType_2:
 	; ld a, (Enemy_1_X)				;
-	ld a, (ix + 5)				;
-	inc a
-	cp 0
-	call z, .hideEnemy
+	ld 		a, (ix + Struct_Enemy.X)				;
+	inc 	a
+	;cp 0
+	call 	z, .hideEnemy
 
 	; ld (Enemy_1_X), a				; save updated value
-	ld (ix + 5), a				; save updated value
+	ld 		(ix + Struct_Enemy.X), a				; save updated value
 
-	push ix
-    ld ix, EnemyTemp_CollisionBox		; update coord of collision box
-    ld (ix + Struct_CollisionBox.X), a
-	pop ix
+	push 	ix
+    ld 		ix, EnemyTemp_CollisionBox		; update coord of collision box
+    ld 		(ix + Struct_CollisionBox.X), a
+	pop 	ix
 
-	jp .contGameLogic
+	jp 		.contGameLogic
 
 .enemyType_3:
 	; ld a, (Enemy_1_X)				;
-	ld 		a, (ix + 6)				;
+	ld 		a, (ix + Struct_Enemy.X)				;
 	;inc 	a
 	cp 		192
 	call 	z, .hideEnemy
 
 	; ld (Enemy_1_X), a				; save updated value
-	ld 		(ix + 6), a				; save updated value
+	ld 		(ix + Struct_Enemy.X), a				; save updated value
 
 	push 	ix
     ld 		ix, EnemyTemp_CollisionBox		; update coord of collision box
@@ -99,9 +100,9 @@ GameLogicEnemy:
 	jp 		.contGameLogic
 
 .hideEnemy:
-	ld a, 0
+	xor		a ; same as ld a, 0
 	; ld (Enemy_1_Show), a			; hide enemy
-	ld (ix), a			; hide enemy
+	ld 		(ix), a			; hide enemy
 	ret
 
 .contGameLogic:
@@ -118,29 +119,29 @@ GameLogicEnemy:
 
 
 	; Check collision between player plane and enemy
-	ld a, (Player_State)
-	cp 0
-	jp nz, .next1				; skip if player isn't alive
+	ld 		a, (Player_State)
+	or		a ; same as cp 0
+	jp 		nz, .next1					; skip if player isn't alive
 
-	ld a, (ix + 5)				; enemy X + 7
-	add 7
-	ld h, a
-	ld a, (ix + 6)				; enemy Y + 7
-	add 7
-	ld l, a
+	ld 		a, (ix + Struct_Enemy.X)	; enemy X + 7
+	add 	7
+	ld 		h, a
+	ld 		a, (ix + Struct_Enemy.Y)	; enemy Y + 7
+	add 	7
+	ld 		l, a
 
-	ld a, (Player_X)
-	ld b, a						; player x1
-	add 16
-	ld d, a						; player x2
+	ld 		a, (Player_X)
+	ld 		b, a						; player x1
+	add 	16
+	ld 		d, a						; player x2
 
-	ld a, (Player_Y)
-	ld c, a						; player y1
-	add 16
-	ld e, a						; player y2
+	ld 		a, (Player_Y)
+	ld 		c, a						; player y1
+	add 	16
+	ld 		e, a						; player y2
 	
-	call CheckCollision             ; 
-    jp nz, .playerPlaneGotHit
+	call 	CheckCollision             ; 
+    jp 		nz, .playerPlaneGotHit
 
 
 .next1:
@@ -150,36 +151,36 @@ GameLogicEnemy:
 	; jp z, .skipCheckEnemy_1
 
 	; TODO: loop throught all 3 player shots and check it against enemy
-   	ld iy, Player_Shot_0_Obj                  ; First shot
+   	ld 		iy, Player_Shot_0_Obj                  ; First shot
 
-   	ld b, 3                                   ; number of shots
+   	ld 		b, 3                                   ; number of shots
 .loop:
-   	push bc
+   	push 	bc
 
-    ld a, (iy + Struct_PlayerShot.Enabled)
-	cp 0
-	jp z, .nextShot
+    ld 		a, (iy + Struct_PlayerShot.Enabled)
+	or		a ; same as cp 0
+	jp 		z, .nextShot
 
 
     ; Test colision between shot and enemy
 	; ld a, (Player_Shot_X)			;   h: x coord
-    ld a, (iy + Struct_PlayerShot.X);   h: x coord
-	add a, 6						;   get the correct upper left pixel
-	ld h, a
+    ld 		a, (iy + Struct_PlayerShot.X);   h: x coord
+	add 	a, 6						;   get the correct upper left pixel
+	ld 		h, a
 	; ld a, (Player_Shot_Y)			;   l: y coord
-    ld a, (iy + Struct_PlayerShot.Y);   l: y coord
-	ld l, a
+    ld 		a, (iy + Struct_PlayerShot.Y);   l: y coord
+	ld 		l, a
 
 	; ld a, (Enemy_1_X)				;   b: x1 coord
-	ld a, (ix + 5)					;   b: x1 coord
-	ld b, a
-    add a, 15                       ;   d: x2 coord
-    ld d, a
+	ld 		a, (ix + Struct_Enemy.X);   b: x1 coord
+	ld 		b, a
+    add 	a, 15                       ;   d: x2 coord
+    ld 		d, a
 	; ld a, (Enemy_1_Y)				;   c: y1 coord
-	ld a, (ix + 6)					;   c: y1 coord
-	ld c, a
-    add a, 15                       ;   e: y2 coord
-    ld e, a
+	ld 		a, (ix + Struct_Enemy.Y);   c: y1 coord
+	ld 		c, a
+    add 	a, 15                       ;   e: y2 coord
+    ld 		e, a
 
     ; ;test
 	; ld h, 80
@@ -189,8 +190,8 @@ GameLogicEnemy:
 	; ld d, 110
 	; ld e, 101
 
-	call CheckCollision             ; 
-    call nz, .CollisionTrue
+	call 	CheckCollision             ; 
+    call 	nz, .CollisionTrue
 
 
 	; push ix
@@ -203,42 +204,42 @@ GameLogicEnemy:
     ; jp c, .colisionTrue
 
 .nextShot:
-	ld bc, Struct_PlayerShot_Size
-	add iy, bc
-	pop bc
-	djnz .loop
+	ld		bc, Struct_PlayerShot_Size
+	add 	iy, bc
+	pop 	bc
+	djnz 	.loop
 
 .skipCheckEnemy_1:
 
 	; ld a, (Enemy_Shot_1_Show)			;
-	ld a, (ix + 8)			;
-	cp 0
-	jp z, .next
+	ld 		a, (ix + Struct_Enemy.Shot_Show)			;
+	or		a ; same as cp 0
+	jp 		z, .next
 
 
 
 
 	; update enemy shot position
 	; ld a, (Enemy_Shot_1_X)				;
-	ld a, (ix + 9)				;
-	ld b, a
+	ld 		a, (ix + Struct_Enemy.Shot_X)				;
+	ld 		b, a
 	; ld a, (Enemy_Shot_1_DeltaX)
-	ld a, (ix + 11)
-	add a, b
-    cp 0
-    jp z, .disableEnemyShot           	;   if x == 0 disable enemy shot
+	ld 		a, (ix + Struct_Enemy.Shot_DeltaX)
+	add 	a, b
+    or		a ; same as cp 0
+    jp 		z, .disableEnemyShot           	;   if x == 0 disable enemy shot
 	
 	; ld (Enemy_Shot_1_X), a				;
-	ld (ix + 9), a				;
+	ld 		(ix + Struct_Enemy.Shot_X), a				;
 
 	; ld a, (Enemy_Shot_1_Y)				;
-	ld a, (ix + 10)				;
-	inc a
-	cp 192
-    jp z, .disableEnemyShot           	;   if y == 192 disable enemy shot
+	ld 		a, (ix + Struct_Enemy.Shot_Y)				;
+	inc 	a
+	cp 		192
+    jp 		z, .disableEnemyShot           	;   if y == 192 disable enemy shot
 
 	; ld (Enemy_Shot_1_Y), a				;
-	ld (ix + 10), a				;
+	ld 		(ix + Struct_Enemy.Shot_Y), a				;
 
 
 
@@ -254,39 +255,38 @@ GameLogicEnemy:
 	; Check collision between enemy shot and player plane
 	; TODO: adjust the box size
 
-	ld a, (Player_State)
-	cp 0
-	jp nz, .next				; skip if player isn't alive
+	ld 		a, (Player_State)
+	or		a ; same as cp 0
+	jp 		nz, .next					; skip if player isn't alive
 
-	ld h, (ix + 9)				; shot X
-	ld l, (ix + 10)				; shot Y
+	ld 		h, (ix + Struct_Enemy.Shot_X)	; shot X
+	ld 		l, (ix + Struct_Enemy.Shot_Y)	; shot Y
 
-	ld a, (Player_X)
-	ld b, a						; player x1
-	add 16
-	ld d, a						; player x2
+	ld 		a, (Player_X)
+	ld 		b, a						; player x1
+	add 	16
+	ld 		d, a						; player x2
 
-	ld a, (Player_Y)
-	ld c, a						; player y1
-	add 16
-	ld e, a						; player y2
+	ld 		a, (Player_Y)
+	ld 		c, a						; player y1
+	add 	16
+	ld 		e, a						; player y2
 
-	call CheckCollision             ; 
-    jp nz, .playerPlaneGotHit
+	call 	CheckCollision             ; 
+    jp 		nz, .playerPlaneGotHit
 
-	jp .next
+	jp 		.next
 
 .playerPlaneGotHit:
 
-	call SoundExplosion
+	call 	SoundExplosion
 
-	ld a, 1
-	ld (Player_State), a
+	ld 		a, 1
+	ld 		(Player_State), a
 
-	ld a, (Player_Lifes)
-	dec a
-	ld (Player_Lifes), a
-	call ShowLifes
+	ld 		hl, Player_Lifes
+	dec 	(hl)
+	call 	ShowLifes
 
 	;ld a, (Player_Lifes)
 	;cp 0
@@ -294,9 +294,9 @@ GameLogicEnemy:
 	
 
 .disableEnemyShot:
-	ld a, 0
+	xor		a ; same as ld a, 0
 	; ld (Enemy_Shot_1_Show), a			;
-	ld (ix + 8), a			;
+	ld 		(ix + Struct_Enemy.Shot_Show), a			;
 
 
 	;rest of logic here
@@ -306,41 +306,32 @@ GameLogicEnemy:
 
 .CollisionTrue:
 
-	call SoundExplosion
+	call 	SoundExplosion
 
-	ld a, 0							; hide enemy
+	xor		a ; ld a, 0							; hide enemy
 	; ld (Enemy_1_Show), a
-	ld (ix), a
-	inc a							; start animation
+	ld 		(ix), a
+	inc 	a							; start animation
 	; ld (Enemy_1_State), a
-	ld (ix + 1), a
+	ld 		(ix + Struct_Enemy.State), a
 
-   	call DisableShot
-    ; ld a, (Player_Shot)
-	; dec a                       	; reset flag of shot fired
-    ; ld (Player_Shot), a         	; 
-
-	; ld d, 0							;   d: x coord
-	; ld e, 192						;   e: y coord		; place sprite off screen
-	; ld a, 63					    ;   a: pattern number (0-63)
-	; ld b, 2							;   b: layer (0-31)
-	; call PutSprite16x16				;   put non existent sprite at layer, to hide the shot
+   	call 	DisableShot
 
 
 	; add points to the score
-	ld hl, (Player_Score)			;
+	ld 		hl, (Player_Score)			;
 	; attention: BCD constants should always be written in hexa
-	ld bc, 0x05						; TODO: get this value correct for each kind of enemy
-	ld d, h							; high byte
-	ld a, l							; low byte
-	add a, c
+	ld 		bc, 0x05						; TODO: get this value correct for each kind of enemy
+	ld 		d, h							; high byte
+	ld 		a, l							; low byte
+	add 	a, c
 	daa
-	jp nc, .notIncScoreHi
-	inc d
+	jp 		nc, .notIncScoreHi
+	inc 	d
 .notIncScoreHi:
-	ld h, d
-	ld l, a
-	ld (Player_Score), hl			; save updated score
+	ld 		h, d
+	ld 		l, a
+	ld 		(Player_Score), hl			; save updated score
 
 	call 	ShowScore
 	; call	UpdateNamesTable_TopStrip
--- gamelogic/gamelogic.s
+++ gamelogic/gamelogic.s
@@ -2,28 +2,28 @@ INCLUDE "GameLogic/EnemyLogic.s"
 
 GameLogic:
 
-   ld iy, Player_Shot_0_Obj                  ; First shot
+   ld    iy, Player_Shot_0_Obj                  ; First shot
 
-   ld b, 3                                   ; number of shots
+   ld    b, 3                                   ; number of shots
 .loop:
-   push bc
+   push  bc
 
-   ld a, (iy + Struct_PlayerShot.Enabled)    ;   get indicator of shot fired
-	; ld a, (Player_Shot)				            ;   get indicator of shot fired
-   cp 0
-   jp z, .nextShot
+   ld    a, (iy + Struct_PlayerShot.Enabled)    ;   get indicator of shot fired
+	; ld  a, (Player_Shot)				            ;   get indicator of shot fired
+   or    a ; same as cp 0
+   jp    z, .nextShot
 
 	; ld a, (Player_Shot_Y)			            ;   get Y position of shot
-	ld a, (iy + Struct_PlayerShot.Y)          ;   get Y position of shot
-   dec a
-   cp TOP_SCREEN_IN_PIXELS - 1
-   call z, .ShotReachesTop                   ;   if y=TOP_SCREEN disable shot
+	ld    a, (iy + Struct_PlayerShot.Y)          ;   get Y position of shot
+   dec   a
+   cp    TOP_SCREEN_IN_PIXELS - 1
+   call  z, .ShotReachesTop                   ;   if y=TOP_SCREEN disable shot
 
-	; ld (Player_Shot_Y), a			            ;   saves updated Y position of shot
-	ld (iy + Struct_PlayerShot.Y), a          ;   saves updated Y position of shot
+	; ld  (Player_Shot_Y), a			            ;   saves updated Y position of shot
+	ld    (iy + Struct_PlayerShot.Y), a          ;   saves updated Y position of shot
 
-   ld ix, Player_Shot_CollisionBox
-   ld (ix + Struct_CollisionBox.Y), a        ; update Y of collision box
+   ld    ix, Player_Shot_CollisionBox
+   ld    (ix + Struct_CollisionBox.Y), a        ; update Y of collision box
 
    ;no need to update X collision box, as it is always the same
 	; ld a, (Player_Shot_X)			            ;   d: x coord
@@ -41,11 +41,11 @@ GameLogic:
    jp    nz, .oddFrame                           ;   alternate colors of shots at each frame
 
    ; even frame
-   ld    a, 14						                  ;   color gray
+   ld    a, COLOR_GREY
    jp    .continue
 
 .oddFrame:
-   ld    a, 8						                  ;   color red
+   ld    a, COLOR_RED
    jp    .continue
 
 
@@ -68,98 +68,97 @@ GameLogic:
 	ld    (Enemy_Shot_4_Color), a
 
 ; check item
-	ld a, (Item_Show)				               ;   
-   cp 0
-   jp z, .enemies                 
+	ld    a, (Item_Show)				               ;   
+   or    a ; same as cp 0
+   jp    z, .enemies                 
 
 
 
 	; Check collision between player plane and item
-	ld a, (Player_State)
-	cp 0
-	jp nz, .skipCheckCollPlaneItem				; skip if player isn't alive
+	ld    a, (Player_State)
+	or    a ; same as cp 0
+	jp    nz, .skipCheckCollPlaneItem				; skip if player isn't alive
 
-	ld a, (Item_X)				; enemy X + 7
-	add 7
-	ld h, a
-	ld a, (Item_Y)				; enemy Y + 7
-	add 7
-	ld l, a
+	ld    a, (Item_X)				; enemy X + 7
+	add   7
+	ld    h, a
+	ld    a, (Item_Y)				; enemy Y + 7
+	add   7
+	ld    l, a
 
-	ld a, (Player_X)
-	ld b, a						; player x1
-	add 16
-	ld d, a						; player x2
+	ld    a, (Player_X)
+	ld    b, a						; player x1
+	add   16
+	ld    d, a						; player x2
 
-	ld a, (Player_Y)
-	ld c, a						; player y1
-	add 16
-	ld e, a						; player y2
+	ld    a, (Player_Y)
+	ld    c, a						; player y1
+	add   16
+	ld    e, a						; player y2
 	
-	call CheckCollision             ; 
-   jp nz, .playerPlaneGotItem
+	call  CheckCollision             ; 
+   jp    nz, .playerPlaneGotItem
 
 
 
 .skipCheckCollPlaneItem:
-	ld hl, (Counter)	    	      ;
+	ld    hl, (Counter)	    	      ;
 
-   bit 0, l
-   jp z, .skipDecY               ; dec Y only at each two frames
+   bit   0, l
+   jp    z, .skipDecY               ; dec Y only at each two frames
 
-	ld a, (Item_Y)			
-   inc a
-   cp 0
-   jp z, .itemReachesBottom
+	ld    a, (Item_Y)			
+   inc   a
+   or    a ; same as cp 0
+   jp    z, .itemReachesBottom
 
-	ld (Item_Y), a
+	ld    (Item_Y), a
 
 .skipDecY:
    ; ld ix, Player_Shot_CollisionBox
    ; ld (ix + Struct_CollisionBox.Y), a
 
-   ld a, COLOR_LIGHT_BLUE        ;   color 1
-	ld (Item_Color), a
+   ld    a, COLOR_LIGHT_BLUE        ;   color 1
+	ld    (Item_Color), a
 
-   bit 0, l
-   jp z, .enemies                ;   alternate colors of item at each frame
+   bit   0, l
+   jp    z, .enemies                ;   alternate colors of item at each frame
 
-   ld a, COLOR_RED			      ;   color 2
-	ld (Item_Color), a
+   ld    a, COLOR_RED			      ;   color 2
+	ld    (Item_Color), a
 
-   jp .enemies
+   jp    .enemies
 
 .itemReachesBottom:
 
-   call DisableItem
-   jp .enemies
+   call  DisableItem
+   jp    .enemies
 
 .playerPlaneGotItem:
-   ld a, (Player_Lifes)
-   inc a
-   ld (Player_Lifes), a
-   call ShowLifes
+   ld    hl, Player_Lifes
+   inc   (hl)
+   call  ShowLifes
    
-   call SoundGetItem
+   call  SoundGetItem
    
-   call DisableItem
+   call  DisableItem
 
-   jp .enemies
+   jp    .enemies
 
 .enemies:
-   ld ix, Enemy_0_Base_Address
-   call GameLogicEnemy
+   ld    ix, Enemy_0_Base_Address
+   call  GameLogicEnemy
 
-   ld ix, Enemy_1_Base_Address
-   call GameLogicEnemy
+   ld    ix, Enemy_1_Base_Address
+   call  GameLogicEnemy
 
-   ld ix, Enemy_2_Base_Address
-   call GameLogicEnemy
+   ld    ix, Enemy_2_Base_Address
+   call  GameLogicEnemy
 
-   ld ix, Enemy_3_Base_Address
-   call GameLogicEnemy
+   ld    ix, Enemy_3_Base_Address
+   call  GameLogicEnemy
 
-   ld ix, Enemy_4_Base_Address
-   call GameLogicEnemy
+   ld    ix, Enemy_4_Base_Address
+   call  GameLogicEnemy
 
    ret
